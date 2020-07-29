210c9a01c81d1297c5dad7bf3514f7d61f6089d1
Nikhil <Nikhilbaldwin08@gmail.com>
2020-07-29 13:59:30 +0530

    Updated 29/07/2020 at 2:00

--- src/views/contextMenu/ControlContextMenu.vue
+++ src/views/contextMenu/ControlContextMenu.vue
@@ -1,149 +1,267 @@
 <template>
   <div>
-    <div class="outer">
-      <div>
-        <i style="font-size:15px" class="fa">&#xf0c4;</i>
-        <span>
+        <div class="outercontext-div">
+          <div class="wrapper-context">
+          <button class="wrapper1-context">
+          <i style="font-size:15px" class="fa">&#xf0c4;</i>
+          <span class="iset-context">
           Cu<u>t</u>
         </span>
-      </div>
-      <div>
-        <i style="font-size:15px" class="fa">&#xf0c5;</i>
-        <span>
+          </button>
+       <button class="wrapper1-context"><i style="font-size:15px" class="fa">&#xf0c5;</i>
+        <span class="iset-context">
           <u>C</u>opy
-        </span>
-      </div>
-      <div>
-        <i style="font-size:15px" class="fa">&#xf0ea;</i>
-        <span>
+        </span></button>
+       <button class="wrapper1-context"><i style="font-size:15px;" class="fa">&#xf0ea;</i>
+        <span class="iset-context">
           <u>P</u>aste
-        </span>
-      </div>
-      <div>
-        <u>D</u>elete
-      </div>
-      <hr />
-      <div>
-        <i style="font-size:15px" class="fa">&#xf187;</i>
-        <span>
+        </span></button>
+       <button class="wrapper1-context">
+         <div></div>
+         <span class="set-context"><u>D</u>elete</span>
+       </button>
+       <div>
+       <hr>
+       </div>
+       <button class="wrapper1-context"><i style="font-size:15px" class="fa">&#xf187;</i>
+        <span class="iset-context">
           Prope<u>r</u>ties
-        </span>
-      </div>
-      <div>
-        View C<u>o</u>de
-      </div>
-      <hr />
-      <div class="dropdown" @click="selectedAlign">
-        <u>A</u>lign
-        <ul class="align dopdown-content" v-if="selectAlign">
-          <li><u>L</u>efts</li>
-          <li><u>C</u>enters</li>
-          <li><u>R</u>ights</li>
-          <hr />
-          <li><u>T</u>ops</li>
-          <li><u>M</u>iddles</li>
-          <li><u>B</u>ottoms</li>
-          <hr />
-          <li>to <u>G</u>rid</li>
-        </ul>
-      </div>
-      <br />
-      <div class="dropdown" @click="selectedMakeSameSize">
-        <u>M</u>ake Same Size
-        <div class="makeSameSize dopdown-content" v-if="selectMakeSameSize">
-          <div><u>W</u>idth</div>
-          <div><u>H</u>eight</div>
-          <div><u>B</u>oth</div>
-        </div>
-      </div>
-      <hr />
-      <div>
-        <u>G</u>roup
-      </div>
-      <div>
-        Bring <u>F</u>orward
-      </div>
-      <div>
-        Send Back<u>w</u>ard
-      </div>
+        </span></button>
+       <button class="wrapper1-context"><div><i style="font-size:15px" class="fa">&#xf1ea;</i></div>
+       <span class="set-context"> View C<u>o</u>de</span>
+       </button>
+       <div>
+       <hr>
+       </div>
+          <button class="wrapper1-context wrapper21"><div></div><ul class="set-context top-level-menu"><li>
+            <a href="#"><u>A</u>lign </a>
+            <ul class="third-level-menu">
+              <li class="wrapper1-context">
+                <div><i style="font-size:13px" class="fa third">&#xf036;</i></div>
+                <a href="#"><u>L</u>efts</a>
+              </li>
+              <li class="wrapper1-context">
+                <div><i style="font-size:13px" class="fa third">&#xf037;</i></div>
+                <a href="#"><u>C</u>enters</a>
+              </li>
+              <li class="wrapper1-context">
+                <div><i style="font-size:13px" class="fa third">&#xf038;</i></div>
+                <a href="#"><u>R</u>ights</a>
+              </li>
+              <hr />
+              <li class="wrapper1-context">
+                <div><i style="font-size:13px" class="fa third">&#xf161;</i></div>
+                <a href="#"><u>T</u>ops</a>
+              </li>
+              <li class="wrapper1-context">
+                <div><i style="font-size:13px" class="fa third">&#xf039;</i></div>
+                <a href="#"><u>M</u>iddles</a>
+              </li>
+              <li class="wrapper1-context">
+                <div><i style="font-size:13px" class="fa third">&#xf160;</i></div>
+                <a href="#"><u>B</u>ottoms</a>
+              </li>
+              <hr />
+              <li class="wrapper1-context">
+                <div><i style="font-size:13px" class="fa third">&#xf00a;</i></div>
+                <a href="#" style="padding-left:15px;">To <u>G</u>rids</a>
+              </li>
+            </ul>
+          </li></ul><div class="triangle-right"></div></button>
+          <button class="wrapper1-context wrapper21"><div></div><ul class="top-level-menu1 set-context">
+          <li>
+            <a href="#"><u>M</u>ake Same Size</a>
+            <ul class="third-level-menu">
+              <li class="wrapper1-context">
+                <div><i style="font-size:13px" class="fa third">&#xf035;</i></div>
+                <a href="#"><u>W</u>idth</a>
+              </li>
+              <li class="wrapper1-context">
+                <div><i style="font-size:13px" class="fa third">&#xf034;</i></div>
+                <a href="#"><u>H</u>eight</a>
+              </li >
+              <li class="wrapper1-context">
+                <div><i style="font-size:13px" class="fa third">&#xf047;</i></div>
+                <a href="#"><u>B</u>oth</a>
+              </li>
+            </ul>
+          </li>
+        </ul><div class="triangle-right"></div></button>
+          <div><hr></div>
+          <button class="wrapper1-context"><div><i style="font-size:15px" class="fa">&#xf247;</i></div><span class="set-context"><u>G</u>roup</span></button>
+          <button class="wrapper1-context"><div><i style="font-size:15px" class="fa">&#xf0c5;</i></div><span class="set-context"> Bring <u>F</u>orward</span></button>
+          <button class="wrapper1-context"><div><i style="font-size:15px" class="fa">&#xf0c5;</i></div><span class="set-context">Send Back<u>w</u>ard</span></button>
+          </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      selectAlign: false,
-      selectMakeSameSize: false
-    };
-  },
-  methods: {
-    selectedAlign() {
-      this.selectAlign = !this.selectAlign;
-    },
-    selectedMakeSameSize() {
-      this.selectMakeSameSize = !this.selectMakeSameSize;
-    }
-  }
-};
+export default {};
 </script>
 
 <style scoped>
-.outer {
-  border: 0.5px solid black;
-  box-shadow: 1px 1px;
-  width: 140px;
+.outercontext-div{
+  border: 0.3px solid black;
+  box-shadow: 2px 2px lightgray;
+  width: 150px;
   height: auto;
-  /* font-family: "Candara"; */
   font-size: 13px;
-  /* position: absolute; */
+  /* margin-left: 300px; */
+}
+
+.wrapper-context {
+  display: grid;
+  grid-template-columns: 1fr;
+  /* grid-row-gap: 4px; */
+  width: 100%;
+  height: 100%;
+}
+.wrapper1-context {
+   display: grid;
+  grid-template-columns: 10% 90%;
+  border: 0.3px solid white;
+  background: white;
+  height: 24px;
+  padding-top: 5px;
+  outline: none;
+}
+.wrapper1-context:hover{
+  background-color: rgb(155, 215, 255);
+  border: 0.3px solid rgb(0, 153, 255);
+}
+.wrapper21{
+  display: grid;
+  grid-template-columns: 10% 85% 5%;
+}
+.triangle-right {
+	width: 0;
+	height: 0;
+	border-top: 5px solid transparent;
+	border-left: 5px solid black;
+  border-bottom: 5px solid transparent;
+  padding-top: 2px;
+}
+.iset-context {
+  text-align: left;
+  padding-left: 15px;
+  font-size: 12px;
+}
+.set-context{
+  text-align: left;
+  padding-left: 15px;
+  font-size: 12px;
+}
+hr{
+  margin: 0px;
+}
+ul{
+  list-style-type: none; 
+    margin-block-start: 0em;
+    margin-block-end: 0em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    padding-inline-start: 0px;
+    margin-top: 0px;
+    margin-bottom: 0px;
+}
+
+
+/* testing submenu */
+
+.top-level-menu {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.top-level-menu > li {
+  position: absolute;
+  float: left;
+  height: 30px;
+  width: 143px;
+}
+
+.top-level-menu li:hover > ul {
+  /* On hover, display the next level's menu */
+  display: inline;
+}
+
+/* Menu Link Styles */
+
+.top-level-menu a /* Apply to all links inside the multi-level menu */ {
+  font-size: 12px;
+  color: black;
+  text-decoration: none;
+  padding: 0 0 0 10px;
+  padding-left: 15px;
+
+  /* Make the link cover the entire list item-container */
+  display: block;
+}
+.top-level-menu a:hover {
+  color: black;
+}
+.third-level-menu {
+  position: absolute;
+  top: 0;
+  right: -100px;
+  width: 110px;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: none;
+  border: 1px solid black;
+  background-color: white;
+}
+
+.third-level-menu > li {
+  height: 20px;
+  padding-top: 5px;
+  /* margin: 1px; */
 
 }
-.outer > div {
-  margin-top: 3px;
-  margin-bottom: 3px;
-  margin-left: 5px;
-  
+.third-level-menu > li:hover {
+  background-color: rgb(155, 215, 255);
+  border: 0.2px solid rgb(0, 153, 255);
 }
-.outer > div:hover {
-  background-color: rgb(146, 184, 255);
-  /* border: 1px solid rgb(69, 155, 253); */
+.top-level-menu1 {
+  list-style: none;
+  padding: 0;
+  margin: 0;
 }
-.outer > div > span {
-  margin-left: 5px;
-}
-.dropdown {
-  position: relative;
-  display: inline-block;
-}
-.dropdown-content {
-  display: none;
+
+.top-level-menu1 > li {
   position: absolute;
-  background-color: #f1f1f1;
-  min-width: 160px;
-  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-  /* z-index: 1; */
+  float: left;
+  height: 30px;
+  width: 143px;
 }
-.align {
-  border: 0.5px solid black;
-  box-shadow: 1px 1px;
-  width: 140px;
-  height: 200px;
-  font-family: "Candara";
-  font-size: 13px;
-  margin-left: 140px;
-  height: auto;
+
+.top-level-menu1 li:hover > ul {
+  /* On hover, display the next level's menu */
+  display: inline;
 }
-.makeSameSize {
-  border: 0.5px solid black;
-  box-shadow: 1px 1px;
-  width: 140px;
-  height: 200px;
-  font-family: "Candara";
-  font-size: 13px;
-  margin-left: 140px;
-  margin-top: 2px;
-  height: auto;
+
+/* Menu Link Styles */
+
+.top-level-menu1 a /* Apply to all links inside the multi-level menu */ {
+  font-size: 12px;
+  color: black;
+  text-decoration: none;
+  padding: 0 0 0 10px;
+  padding-left: 15px;
+
+  /* Make the link cover the entire list item-container */
+  display: block;
+}
+.top-level-menu1 a:hover {
+  color: black;
+}
+.third{
+  padding-left: 5px;
+}
+.third>a{
+  padding-left: 10px;
 }
 </style>
--- src/views/contextMenu/UserformContextMenu.vue
+++ src/views/contextMenu/UserformContextMenu.vue
@@ -1,45 +1,95 @@
 <template>
   <div>
-      <div class="outerdiv">
-          <div><span class="set">Select <u>A</u>ll</span></div>
-          <div><i style='font-size:15px' class='fa'>&#xf0ea;</i><span class="iset"><u>P</u>aste</span></div>
-          <div><span class="set"><u>D</u>elete</span></div>
-          <hr>
-          <div><i style='font-size:15px' class='fa'>&#xf187;</i><span class="iset">Prope<u>r</u>ties</span></div>
-          <div><span class="set">View C<u>o</u>de</span></div>
-          <div><span class="set">T<u>a</u>b Order</span></div>
+    <div class="outercontext-div">
+      <div class="wrapper-context">
+        <button class="wrapper1-context">
+          <div>
+            <i style="font-size:15px" class="fa">&#xf07b;</i>
+          </div>
+          <span class="set-context">
+            Select
+            <u>A</u>ll
+          </span>
+        </button>
 
+        <button class="wrapper1-context">
+          <i style="font-size:15px" class="fa">&#xf0ea;</i>
+          <span class="iset-context">
+            <u>P</u>aste
+          </span>
+        </button>
+        <div>
+          <hr />
+        </div>
+        <button class="wrapper1-context">
+          <i style="font-size:15px" class="fa">&#xf187;</i>
+          <span class="iset-context">
+            Prope<u>r</u>ties
+          </span>
+        </button>
+        <button class="wrapper1-context">
+          <div>
+            <i style="font-size:15px" class="fa">&#xf1ea;</i>
+          </div>
+          <span class="set-context">
+            View C<u>o</u>de
+          </span>
+        </button>
+        <button class="wrapper1-context">
+          <div></div>
+          <span class="set-context">
+            T<u>a</u>b Order
+          </span>
+        </button>
       </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style scoped>
-.outerdiv{
-    border: 0.5px solid black;
-    box-shadow: 1px 1px;
-    width: 140px;
-    height: auto;
-    font-size: 13px;
+.outercontext-div {
+  border: 0.3px solid black;
+  box-shadow: 2px 2px lightgray;
+  width: 150px;
+  height: auto;
+  font-size: 13px;
+  /* margin-left: 300px; */
 }
-.outerdiv > div {
-  margin-top: 3px;
-  margin-bottom: 3px;
-  margin-left: 5px;
+.wrapper-context {
+  display: grid;
+  grid-template-columns: 1fr;
+  /* grid-row-gap: 4px; */
+  width: 100%;
+  height: 100%;
 }
-.outerdiv > div:hover {
-  background-color: rgb(146, 184, 255);
-  /* border: 1px solid rgb(69, 155, 253); */
+.wrapper1-context {
+  display: grid;
+  grid-template-columns: 10% 90%;
+  border: 0.3px solid white;
+  background: white;
+  height: 24px;
+  padding-top: 5px;
+  outline: none;
 }
-.iset{
-  margin-left: 5px;
+.wrapper1-context:hover {
+  background-color: rgb(155, 215, 255);
+  border: 0.3px solid rgb(0, 153, 255);
 }
-.set{
-  margin-left: 20px;
+.iset-context {
+  text-align: left;
+  padding-left: 15px;
+  font-size: 12px;
+}
+.set-context {
+  text-align: left;
+  padding-left: 15px;
+  font-size: 12px;
+}
+hr {
+  margin: 0px;
 }
 </style>
